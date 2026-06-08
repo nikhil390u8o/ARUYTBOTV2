ea9c8ffc5c781b3e2e7b78d7cf40738892f15346
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-06-08 15:19:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def btn(text, callback_data=None, url=None, web_app=None, style=None):
 # ── ᴄᴏɴꜰɪɢ ───────────────────────────────────────────────────────
 API_ID     = int(os.environ.get("API_ID",     "20898349"))
 API_HASH   = os.environ.get("API_HASH",       "9fdb830d1e435b785f536247f49e7d87")
-BOT_TOKEN  = os.environ.get("BOT_TOKEN",      "8808019830:AAFwOYjpCNGmY_om3vJ9Q0jpe34n6QAN-zA")
+BOT_TOKEN  = os.environ.get("BOT_TOKEN",      "8808019830:AAEE05lxb1tIVLPWsMIfsLKYuR8NBPqwB6I")
 CHANNEL_ID = os.environ.get("CHANNEL_ID",     "@sxypndu")
 MASTER_KEY = os.environ.get("MASTER_KEY",     "YukiMasterAdmin2026")
 API_BASE   = os.environ.get("API_BASE",       "https://pandaapiv2.up.railway.app")
